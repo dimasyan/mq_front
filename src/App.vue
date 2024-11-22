f9744026a7b24cba6cb0cddb7669ed7e09f55483
dimasyan <dimash_lpfan@mail.ru>
2024-11-22 12:06:26 +0500

    dark mode config

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 import { onMounted } from 'vue'
+import { useWebAppTheme } from 'vue-tg'
 
 const applyDarkMode = () => {
-  const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+  const prefersDark = useWebAppTheme().colorScheme === 'dark'
   const html = document.documentElement;
 
   if (prefersDark) {
