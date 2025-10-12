ddbcedeaecda9f6bde9fb095beb7f859377a290d
dimasyan <dimash_lpfan@mail.ru>
2025-10-12 13:39:50 +0500

    new game

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -210,7 +210,7 @@ function clickOutside(e: MouseEvent) {
 
           <div class="bg-gradient-to-r from-purple-400 to-indigo-500 rounded-3xl shadow-xl p-8 sm:p-12 text-center transform hover:scale-105 transition-all duration-300">
             <h3 class="text-3xl sm:text-4xl text-gray-900 mb-6">
-              <span class="inline-block bg-gradient-to-r from-white to-indigo-200 text-transparent bg-clip-text font-bold">Ultra Music Mix #15: KZ и Зарубежка</span>
+              <span class="inline-block bg-gradient-to-r from-white to-indigo-200 text-transparent bg-clip-text font-bold">Ultra Music Mix #16: Все эпохи и жанры</span>
             </h3>
 
             <!-- Game Info with Icons -->
@@ -218,13 +218,13 @@ function clickOutside(e: MouseEvent) {
               <!-- Date -->
               <div class="flex items-center justify-start sm:justify-center gap-2">
                 <span class="text-3xl text-purple-600">📅</span>
-                <p class="text-gray-100 font-semibold">7 октября 2025, 19:30</p>
+                <p class="text-gray-100 font-semibold">16 октября 2025, 19:30</p>
               </div>
 
               <!-- Location -->
               <div class="flex items-center justify-start sm:justify-center gap-3">
                 <span class="text-3xl text-purple-600">📍</span>
-                <p class="text-gray-200 font-semibold text-left sm:text-center">Хмельница, <br class="hidden sm:block"> ул. Карасай Батыра 193</p>
+                <p class="text-gray-200 font-semibold text-left sm:text-center">лаунж-бар Avenue, <br class="hidden sm:block"> ул. Мынбаева 53</p>
               </div>
 
               <!-- Status -->
@@ -236,7 +236,7 @@ function clickOutside(e: MouseEvent) {
 
             <!-- Register Button -->
             <button
-              @click="openRegistration('Ultra Music Mix#15')"
+              @click="openRegistration('Ultra Music Mix#16')"
               class="bg-purple-600 text-white text-2xl font-bold mt-4 px-8 py-4 rounded-full hover:bg-purple-700 transition-transform transform hover:scale-105 shadow-md"
             >
               Записаться
