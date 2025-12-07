fe21010b7d534428e427296fff19c9ba68947e42
dimasyan <dimash_lpfan@mail.ru>
2025-12-07 23:30:16 +0500

    new game

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -210,7 +210,7 @@ function clickOutside(e: MouseEvent) {
 
           <div class="mb-8 bg-gradient-to-r from-purple-400 to-indigo-500 rounded-3xl shadow-xl p-8 sm:p-12 text-center transform hover:scale-105 transition-all duration-300">
             <h3 class="text-3xl sm:text-4xl text-gray-900 mb-6">
-              <span class="inline-block bg-gradient-to-r from-white to-indigo-200 text-transparent bg-clip-text font-bold">Ultra Music Mix #25: Все эпохи и жанры</span>
+              <span class="inline-block bg-gradient-to-r from-white to-indigo-200 text-transparent bg-clip-text font-bold">Ultra Music Mix #26: 90е и 00е</span>
             </h3>
 
             <!-- Game Info with Icons -->
@@ -218,7 +218,7 @@ function clickOutside(e: MouseEvent) {
               <!-- Date -->
               <div class="flex items-center justify-start sm:justify-center gap-2">
                 <span class="text-3xl text-purple-600">📅</span>
-                <p class="text-gray-100 font-semibold">4 декабря 2025, 19:30</p>
+                <p class="text-gray-100 font-semibold">11 декабря 2025, 19:30</p>
               </div>
 
               <!-- Location -->
