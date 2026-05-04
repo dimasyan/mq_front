d44ccdf0e3009600408ea6be57f666a6a2a1223e
dimasyan <dimash_lpfan@mail.ru>
2026-05-04 17:30:20 +0500

    new month

--- src/utils/games.ts
+++ src/utils/games.ts
@@ -1,65 +1,65 @@
 export const GAMES = [
   {
-    id: 48,
-    name: 'Ultra Music Mix#48: Зарубежная музыка',
-    shortName: 'Ultra Music Mix#48: Зарубежная музыка',
-    date: '9 апреля 2026, 19:30',
+    id: 55,
+    name: 'Ultra Music Mix#55: Все эпохи и жанры',
+    shortName: 'Ultra Music Mix#55: Все эпохи и жанры',
+    date: '7 мая, 19:30',
     venue: 'Avenue',
     address: ' ул. Мынбаева 53'
   },
   {
-    id: 49,
-    name: 'Ultra Music Mix#49: Rap vs Rock',
-    shortName: 'Ultra Music Mix#49: Rap vs Rock',
-    date: '11 апреля 2026, 17:00',
+    id: 56,
+    name: 'Ultra Music Mix#56: 2000e и 2010е',
+    shortName: 'Ultra Music Mix#56: 2000e и 2010е',
+    date: '14 мая, 19:30',
     venue: 'Avenue',
     address: ' ул. Мынбаева 53'
   },
   {
-    id: 50,
-    name: 'Ultra Music Mix#50: Все эпохи и жанры',
-    shortName: 'Ultra Music Mix#50: Все эпохи и жанры',
-    date: '16 апреля 2026, 19:30',
+    id: 57,
+    name: 'Ultra Music Mix#57: Rap & RnB',
+    shortName: 'Ultra Music Mix#57: Rap & RnB',
+    date: '16 мая, 17:00',
     venue: 'Avenue',
     address: ' ул. Мынбаева 53'
   },
   {
-    id: 51,
-    name: 'Ultra Music Mix#51: Rap и RnB',
-    shortName: 'Ultra Music Mix#51: Rap и RnB',
-    date: '18 апреля 2026, 17:00',
+    id: 58,
+    name: 'Ultra Music Mix#58: Все эпохи и жанры',
+    shortName: 'Ultra Music Mix#58: Все эпохи и жанры',
+    date: '21 мая, 19:30',
     venue: 'Avenue',
     address: ' ул. Мынбаева 53'
   },
   {
-    id: 52,
-    name: 'Кино и Музыка #4',
-    shortName: 'Кино и Музыка #4',
-    date: '23 апреля 2026, 19:30',
+    id: 1,
+    name: 'LINKIN PARK',
+    shortName: 'LINKIN PARK ',
+    date: '23 мая, 17:00',
     venue: 'Avenue',
     address: ' ул. Мынбаева 53'
   },
   {
-    id: 53,
-    name: 'Ultra Music Mix#52: Rock',
-    shortName: 'Ultra Music Mix#52: Rock',
-    date: '26 апреля 2026, 16:30',
+    id: 59,
+    name: 'Ultra Music Mix#59: Rock',
+    shortName: 'Ultra Music Mix#59: Rock',
+    date: '27 мая, 17:00',
+    venue: 'Avenue',
+    address: ' ул. Мынбаева 53'
+  },
+  {
+    id: 5,
+    name: 'Кино и Музыка#5',
+    shortName: 'Кино и Музыка#5',
+    date: '28 мая, 19:30',
     venue: 'Avenue',
     address: ' ул. Мынбаева 53'
   },
   {
     id: 54,
-    name: 'Ultra Music Mix#53: Все эпохи и жанры',
-    shortName: 'Ultra Music Mix#53: Все эпохи и жанры',
-    date: '30 апреля 2026, 19:30',
-    venue: 'Avenue',
-    address: ' ул. Мынбаева 53'
-  },
-  {
-    id: 55,
-    name: 'Ultra Music Mix#54: KZ и Зарубежка',
-    shortName: 'Ultra Music Mix#54: KZ и Зарубежка',
-    date: '2 мая 2026, 17:00',
+    name: 'Ultra Music Mix#60: 90е и 00е',
+    shortName: 'Ultra Music Mix#60: 90е и 00е',
+    date: '4 июня, 19:30',
     venue: 'Avenue',
     address: ' ул. Мынбаева 53'
   },
